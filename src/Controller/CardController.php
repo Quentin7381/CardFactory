e8245de70d8e80cef23cdf41e094d28d0037fb49
Quentin Avrain <quentin.avrain@gmail.com>
2025-04-22 12:27:24 +0200

    Card : edit

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -77,9 +77,63 @@ final class CardController extends AbstractController
                 // Set the image path in the entity
                 $card->setCardImage($relativePath);
             }
-            
+
             // Save the card
             $this->entityManager->persist($card);
+            $this->entityManager->flush();
+
+            // Redirect to the list page
+            return $this->redirectToRoute('app_card_list');
+        }
+
+        // Render the form
+        return $this->render('card/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/cards/edit/{id}', name: 'app_card_edit')]
+    public function edit(Request $request, Card $card, SluggerInterface $slugger): Response
+    {
+        $user = $this->getUser();
+
+        if (!$user) {
+            $this->addFlash('error', 'You must be logged in to view this page.');
+            return $this->redirectToRoute('app_login');
+        }
+
+        if ($card->getAuthor() !== $user) {
+            throw $this->createAccessDeniedException('You do not have permission to edit this card.');
+        }
+
+        // Get the form
+        $form = $this->createForm(CardType::class, $card);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $imageFile = $form->get('card_image')->getData();
+
+            if ($imageFile) {
+                $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
+
+                try {
+                    $imageFile->move(
+                        $this->getParameter('images_directory'), // Define this parameter in services.yaml
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // Handle exception if something happens during file upload
+                }
+
+                $relativePath = 'uploads/images/' . $newFilename;
+
+                // Set the new image path in the entity
+                $card->setCardImage($relativePath);
+            }
+
+            // Save the card
             $this->entityManager->flush();
 
             // Redirect to the list page
